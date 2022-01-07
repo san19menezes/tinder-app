67a9f000c5989c698bb8ade3f7a838ab29a9e1cc
san19menezes <97026463+san19menezes@users.noreply.github.com>
2022-01-07 18:09:51 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
-import "./styles.css";
+import React, { useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Person from "./components/Person";
+import Lonely from "./components/Lonely";
+import data from "./data.json";
 
 export default function App() {
+  const [people, setPeople] = useState(data);
+  const [likedUsers, setLikedUsers] = usseState([]);
+  const [superLikedUsers, setSuperLikedUsers] = useState([]);
+  const [dislikedUsers, setDislikedUsers] = useState([]);
+  const activeUser = 0;
+
+  switch (action) {
+    case "ADD_TO_LIKED_USER":
+      break;
+    case "ADD_TO_DISLIKED_USER":
+      break;
+    case "ADD_TO_SUPERLIKED_USER":
+      break;
+    default:
+      return people;
+  }
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className="app">
+      <Header />
+      {people[1] ? (
+        <Person
+          key={person[1].id}
+          person={people[1]}
+          modifySufercialChoices={modifySuperficialChoices}
+          likedUsers={likedUsers}
+        />
+      ) : (
+        <Lonely
+          activeUsersImage={people[activeUser].image}
+          likedUsers={likedUsers}
+          superLikedUsers={superLikedUsers}
+        />
+      )}
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Logo from "./Logo";
+
+const Header = () => {
+  <header>
+    <div className="fl"></div>
+  </header>;
+};
